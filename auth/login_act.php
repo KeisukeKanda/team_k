999b5652f1636227c866a36de4ce26d8da49e559
keita_kawaguchi <keikawagu@gmail.com>
2021-06-23 15:57:13 +0900

    認証処理ファイルにコメントを追加

--- auth/login_act.php
+++ auth/login_act.php
@@ -2,26 +2,36 @@
 
 session_start();
 
+// DB接続とfancs.phpを読み込み
 require("../db/database.php");
 require("../funcs.php");
 
-    $email = $_POST['login_id'];
-    $password = $_POST['password'];
 
+// ログインフォームで入力した値の受け取り
+$email = $_POST['login_id'];
+$password = $_POST['password'];
+
+
+    //空白を許可しない
     if (!empty($_POST['login_id'])) {
+
+        // csrf対策
         if ($_POST['csrfToken'] === $_SESSION['csrfToken']) {
             $sql = "SELECT * FROM users WHERE email=:email";
             $stmt = $pdo->prepare($sql);
             $stmt->bindValue(':email', $email);
             $res = $stmt->execute();
-
             if ($res==false) {
                 sql_error($stmt);
             }
 
+            // ログインユーザー情報のみをfetchで取得
             $val = $stmt->fetch();
 
+            // 入力されたパスワードとハッシュ化されたパスワードを照合
             if (password_verify($password, $val["password"]) === true) {
+
+                // セッションIDとログインユーザー名をセッションに保存
                 if ($val["user_id"] !="") {
                     $_SESSION["chk_ssid"] = session_id();
                     $_SESSION["name"] = $val["name"];
