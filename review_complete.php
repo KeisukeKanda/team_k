4aba6068d19b6044bd7a8ee6d973768267ec0864
Keisuke Kanda <keisukekanda@gmail.com>
2021-06-23 12:42:22 +0900

    review_complete.phpを作成した

---
+++ review_complete.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>レビュー登録後画面</title>
+</head>
+<body>
+  <h1>レビューのご登録ありがとうございます。</h1>
+  <button><a href="#">Homeへ戻る</a></button>
+</body>
+</html>
