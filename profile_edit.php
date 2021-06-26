1e67f269052baa4816ca4c9321ee4174faa52e87
keita_kawaguchi <keikawagu@gmail.com>
2021-06-26 23:50:27 +0900

    プロフィール画面を編集

--- profile_edit.php
+++ profile_edit.php
@@ -70,6 +70,7 @@ $val = $res->fetch();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/reset.css">
     <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/profile.css">
     <title>Document</title>
 </head>
 
