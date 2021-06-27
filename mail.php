f7d86ff2ef37d4ec1f82c1b4542e00d970e22adb
keita_kawaguchi <keikawagu@gmail.com>
2021-06-27 20:50:24 +0900

    メール画面レイアウト作成

--- mail.php
+++ mail.php
@@ -1,27 +1,49 @@
-<html>
-<head>
-  <meta charset="utf-8" />
-</head>
-<body>
-<?php
-  mb_language("Japanese");
-  mb_internal_encoding("UTF-8");
+  <!DOCTYPE html>
+  <html lang="ja">
 
-  $to = $_POST['to'];
-  $title = $_POST['title'];
-  $message = $_POST['content'];
-  $headers = "From: team_k@team_k.com";
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/all.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/footer.css">
+    <link rel="stylesheet" href="./css/mail.css">
+    <title>ISEKAI</title>
+  </head>
 
-  if(mb_send_mail($to, $title, $message, headers))
-  {
-    echo "メール送信成功です";
-  }
-  else
-  {
-    echo "メール送信失敗です";
-  }
- ?>
- <a href="host_index.php">Host Indexへ戻る</a>
+  <body>
+    <div class="warp">
+      <!-- componentフォルダからヘッダーを読み込み -->
+      <?php include("component/header.php") ?>
 
-</body>
-</html>
+      <div class="message">
+        <?php
+      mb_language("Japanese");
+      mb_internal_encoding("UTF-8");
+
+      $to = $_POST['to'];
+      $title = $_POST['title'];
+      $message = $_POST['content'];
+      $headers = "From: team_k@team_k.com";
+
+      if (mb_send_mail($to, $title, $message, headers)) {
+          echo "メール送信成功です";
+      } else {
+          echo "メール送信失敗です";
+      }
+      ?>
+      </div>
+
+      <div class="backhome">
+        <a href="host_index.php">ホスト管理画面へ戻る</a>
+      </div>
+
+    </div>
+
+    <!-- フッターを呼び出し -->
+    <?php include("component/footer.php") ?>
+  </body>
+
+  </html>
--- mail_input.php
+++ mail_input.php
@@ -1,13 +1,42 @@
-<html>
+<!DOCTYPE html>
+<html lang="ja">
+
 <head>
-  <meta charset="utf-8" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="./css/all.css">
+  <link rel="stylesheet" href="./css/header.css">
+  <link rel="stylesheet" href="./css/footer.css">
+  <link rel="stylesheet" href="./css/mail_input.css">
+  <title>ISEKAI</title>
 </head>
+
 <body>
-  <form action="mail.php" method="post">
-    <p>送り先</p><input type="text" name="to">
-    <p>件名</p><input type="text" name="title">
-    <p>メッセージ</p><textarea name="content" cols="60" rows="10"></textarea>
-    <p><input type="submit" name="send" value="送信"></p>
-  </form>
+  <div class="warp">
+    <!-- componentフォルダからヘッダーを読み込み -->
+    <?php include("component/header.php") ?>
+
+    <div class="mail-form">
+      <div class="box">
+
+        <form action="mail.php" method="post">
+          <div class="form-title">送り先</div>
+          <input type="text" class="mail-inputform" name="to">
+          <div class="form-title">件名</div>
+          <input type="text" class="mail-inputform" name="title">
+          <div class="form-title">メッセージ</div>
+          <textarea name="content" cols="60" rows="10"></textarea>
+          <div><input type="submit" class="mail-submit-btn" name="send" value="送信"></div>
+        </form>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- フッターを呼び出し -->
+  <?php include("component/footer.php") ?>
 </body>
+
 </html>
