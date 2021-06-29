d6d7b7b93e0a55fb2a4bc48fb09feaa4e0589822
kankan <shiraishi-k@clothesoperation.co.jp>
2021-06-29 11:51:28 +0900

    残り表示画面へ統合のみ

--- review_show.php
+++ review_show.php
@@ -16,7 +16,7 @@ if ($status == false) {
     sql_error($stmt);
 } else {
     while($result = $stmt->fetch(PDO::FETCH_BOTH)){
-print $result[0];}
+$contents_review=$result[0];}
 }
 
 
@@ -29,7 +29,7 @@ if ($status == false) {
     sql_error($stmt);
 } else {
     while($result = $stmt->fetch(PDO::FETCH_BOTH)){
-print $result[0];}
+$hospitality_review=$result[0];}
 }
 
 
@@ -42,7 +42,7 @@ if ($status == false) {
     sql_error($stmt);
 } else {
     while($result = $stmt->fetch(PDO::FETCH_BOTH)){
-print $result[0];}
+$communication_review=$result[0];}
 }
 
 
@@ -55,10 +55,31 @@ if ($status == false) {
     sql_error($stmt);
 } else {
     while($result = $stmt->fetch(PDO::FETCH_BOTH)){
-print $result[0];}
+$price_review=$result[0];}
 }
 
 
-
+$total_review=($price_review + $communication_review + $hospitality_review +contents_review)/4;
 
 ?>
+
+
+
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Sample</title>
+</head>
+<body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="./raty-3.0.0/lib/jquery.raty.js"></script>
+
+  <div></div>
+  <script type="text/javascript">
+  $('div').raty({ readOnly: true, score: <?=$total_review?> });
+  </script>
+
+</body>
+</html>
