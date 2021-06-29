f1636225348e228c53b7942c874fbc411b2aeccf
kankan <shiraishi-k@clothesoperation.co.jp>
2021-06-30 02:17:52 +0900

    スカイウェイ一旦CSS終わり

--- service_main.php
+++ service_main.php
@@ -26,11 +26,14 @@ $project_id=$_GET["project_id"];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
+    <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/service_main.css">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/header.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 
-
+<?php include("component/header.php") ?>
 
 
 <div class="container">
@@ -41,31 +44,31 @@ $project_id=$_GET["project_id"];
             </p>
             <div class="room">
                 <div class="gamen_all">
-                    <p class="gamen_title">通話画面<br>（room名を共有してください）</p>
-                        <video id="js-local-stream" class="gamen"></video>
-                        <span hidden id="js-room-mode"></span>
-                        <div class="gamen_pass">
-                            <input type="text" placeholder="Room Name" id="js-room-id">
-                            <button id="js-join-trigger">Join</button>
-                            
-                            <button  id="show">Leave</button>
-                        <!-- テスト中 -->
-                            <!-- レイヤー -->
-                            <div id="layer"></div>
-                            <!-- ポップアップ -->
-                            <div id="popup">
-                                <div>通話を終了してもよろしいですか？</div>
-                                <input type="button" id="js-leave-trigger" value="はい">
-                                <input type="button" id="back_coll" value="いいえ">
+                    <p class="gamen_title">通話画面（room名を共有してください）<input type="text" placeholder="Room Name" id="js-room-id"></p>
+                            <div class="gamen_pass">
+                                <div class="join_btn">
+                                    <button id="js-join-trigger" class="btn">Join</button>
+                                    <button  id="leave_button" class="btn">Leave</button>
+                                </div>
+                            <div id="layer">
+                                
+                            </div>
+                            <div class="yoko">
+                                <video id="js-local-stream" class="gamen zibun"></video>
+                                <span hidden id="js-room-mode"></span>
+                                <div class="remote-streams aite" id="js-remote-streams"></div>
                             </div>
 
-
-
-                            <!-- <div><button id="js-leave-trigger">はい</button></div> -->
+                            <!-- ポップアップ -->
+                            <div id="popup">
+                                <div><p class="btn1">通話を終了してもよろしいですか？</p></div>
+                                <p class="btn1"><input type="button" id="js-leave-trigger" class="btn" value="はい"><br></p>
+                                <p class="btn1"><input type="button" id="back_coll" class="btn" value="いいえ"></p>
+                            </div>
                         </div>
                 </div>
 
-                <div class="remote-streams" id="js-remote-streams"></div>
+                
 
                 <div>
                     <pre class="messages" id="js-messages"></pre>
@@ -80,7 +83,9 @@ $project_id=$_GET["project_id"];
 
 
 <!-- とりあえずreview.phpに遷移させる -->
-<p id="go_review">ご利用ありがとうございました。よろしければレビューの記入にご協力ください。<br><a href="review.php">レビューにすすむ</a></p>
+<p id="go_review">ご利用ありがとうございました。よろしければレビュー記入にご協力ください。<br>
+    <input type="button" id="go_to_review" class="btn" value="レビューにすすむ">
+</p>
 
     <!-- ここからスカイウェイのスクリプト -->
     <script src="//cdn.webrtc.ecl.ntt.com/skyway-4.4.1.js"></script>
@@ -91,21 +96,37 @@ $project_id=$_GET["project_id"];
     <!-- ここからモーダルテスト -->
     <script>
     $(function() {
+    $('#js-join-trigger').click(function(e) {
+        $('#leave_button').show();
+        $('#js-local-stream').removeClass('zibun');
+        $('#js-local-stream').addClass('zibun2');
+    });
+
+
+
     // show popupボタンクリック時の処理
-    $('#show').click(function(e) {
+    $('#leave_button').click(function(e) {
         $('#popup, #layer').show();
     });
 
     // レイヤー/ポップアップのcloseボタンクリック時の処理
     $('#js-leave-trigger, #layer').click(function(e) {
         $('#popup, #layer').hide();
+        $('#js-local-stream').hide();
+        $('.gamen_all').hide();
+        $('.messages').hide();
         $('#go_review').show();
     });
     $('#back_coll').click(function(e) {
         $('#popup, #layer').hide();
     });
 
+    $('#go_to_review').click(function(e) {
+        window.location.href = 'review.php';
+    });
 
+
+    
 });
 </script>
 </div>
