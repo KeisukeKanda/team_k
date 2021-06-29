c3732183359da02eb7506c07afd361737a92820c
keita_kawaguchi <keikawagu@gmail.com>
2021-06-29 15:18:39 +0900

    ヘッダーとプロフィール編集エリアの修正

--- component/header.php
+++ component/header.php
@@ -1,31 +1,31 @@
-        <div class="header">
-            <!-- ヘッダー -->
-            <div class="header-box">
-                <div class="logo">
-                    <a href="index.php">ISEKAI</a>
-                </div>
-                <div class="nav-box">
-                    <ul class="menu">
-                        <?php if ($user_id == 0): ?>
-                        <li class="menu-list"><a href="auth/signup.php">サインアップ</a></li>
-                        <li class="menu-list"><a href="auth/login.php">ログイン</a></li>
-                        <?php else: ?>
-                        <!-- <li class="menu-list">
-                            こんにちは、<?= $username ?>
-                        </li> -->
-                        <li class="menu-list"><a href="profile.php">マイプロフィール</a></li>
-                        <li class="menu-list"><a href="user_schedule.php">予約一覧</a></li>
-                        <li class="menu-list"><a href="favorites.php">お気に入り一覧</a></li>
-
-                        <!-- ログインユーザーがすでにhost登録した場合のみ表示 -->
-                        <?php if ($val["host"] == 1): ?>
-                        <li class="menu-list"><a href="host_index.php">ホスト管理画面</a></li>
-                        <?php endif; ?>
-
-                        <li class="menu-list"><a href="auth/logout.php">ログアウト</a></li>
-                        <?php endif; ?>
-                    </ul>
-                </div>
-            </div>
-            <!-- ヘッダー終わり -->
+<div class="header">
+    <!-- ヘッダー -->
+    <div class="header-box">
+        <div class="logo">
+            <a href="index.php">ISEKAI</a>
         </div>
+        <div class="nav-box">
+            <ul class="menu">
+                <?php if ($user_id == 0): ?>
+                <li class="menu-list"><a href="auth/signup.php">サインアップ</a></li>
+                <li class="menu-list"><a href="auth/login.php">ログイン</a></li>
+                <?php else: ?>
+                <!-- <li class="menu-list">
+                            こんにちは、<?= $username ?>
+                </li> -->
+                <li class="menu-list"><a href="profile.php">マイプロフィール</a></li>
+                <li class="menu-list"><a href="user_schedule.php">予約一覧</a></li>
+                <li class="menu-list"><a href="favorites.php">お気に入り一覧</a></li>
+
+                <!-- ログインユーザーがすでにhost登録済みの場合のみ表示 -->
+                <?php if ($val["host"] == 1): ?>
+                <li class="menu-list"><a href="host_index.php">ホスト管理画面</a></li>
+                <?php endif; ?>
+
+                <li class="menu-list"><a href="auth/logout.php">ログアウト</a></li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+    <!-- ヘッダー終わり -->
+</div>
