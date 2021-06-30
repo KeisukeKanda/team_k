8adad8c2ac056954d1f19ce7ed5f335ffab22f83
keita_kawaguchi <keikawagu@gmail.com>
2021-06-30 14:16:35 +0900

    レビュー周りの修正

--- user_schedule_detail.php
+++ user_schedule_detail.php
@@ -8,11 +8,11 @@ require("db_set/db.php");
 
 //セッションハイジャック対策
 if (!isset($_SESSION["chk_ssid"]) || $_SESSION["chk_ssid"] != session_id()) {
-  echo "Login Error!";
-  exit();
+    echo "Login Error!";
+    exit();
 } else {
-  session_regenerate_id(true);
-  $_SESSION["chk_ssid"] = session_id();
+    session_regenerate_id(true);
+    $_SESSION["chk_ssid"] = session_id();
 }
 
 //ユーザー名を取得
@@ -20,9 +20,9 @@ $username = $_SESSION["name"];
 $user_id = $_SESSION["user_id"];
 
 if (!isset($_GET["reservation_id"]) || $_GET["reservation_id"] == "") {
-  exit("ParamError!");
+    exit("ParamError!");
 } else {
-  $reservation_id = intval($_GET["reservation_id"]); //intval数値変換
+    $reservation_id = intval($_GET["reservation_id"]); //intval数値変換
 }
 
 //２．データ抽出SQL作成
@@ -35,11 +35,11 @@ $status = $stmt->execute();
 //３．データ表示
 $view = "";
 if ($status == false) {
-  //execute（SQL実行時にエラーがある場合）
-  $error = $stmt->errorInfo();
-  exit("ErrorQuery:" . $error[2]);
+    //execute（SQL実行時にエラーがある場合）
+    $error = $stmt->errorInfo();
+    exit("ErrorQuery:" . $error[2]);
 } else {
-  $row = $stmt->fetch();
+    $row = $stmt->fetch();
 }
 
 
@@ -69,18 +69,26 @@ if ($status == false) {
       <h1>予約詳細</h1>
       <div class="user_reservation">
         <div class="user_reservation_list">
-          <div><img src="project_img/<?= $row["project_img"] ?>" width="300"></div>
+          <div><img
+              src="project_img/<?= $row["project_img"] ?>"
+              width="300"></div>
           <div>
-            <div class="project_title"><i class="fas fa-globe"></i> <?= $row["title"] ?></div>
-            <div class="project_contents"><i class="fas fa-table"></i> <?= $row["date"] . " " . $row["reservation_time"] ?></div>
+            <div class="project_title"><i class="fas fa-globe"></i> <?= $row["title"] ?>
+            </div>
+            <div class="project_contents"><i class="fas fa-table"></i> <?= $row["date"] . " " . $row["reservation_time"] ?>
+            </div>
             <div class="project_contents">○ カテゴリー</div>
-            <div class="project_contents">　<?= $row["category"] ?></div>
+            <div class="project_contents">　<?= $row["category"] ?>
+            </div>
             <div class="project_contents">○ 案内する場所</div>
-            <div class="project_contents">　<?= $row["country"] . " " . $row["project_area"] ?></div>
+            <div class="project_contents">　<?= $row["country"] . " " . $row["project_area"] ?>
+            </div>
             <div class="project_contents">○ 内容</div>
-            <div class="project_contents">　<?= $row["experience"] ?></div>
+            <div class="project_contents">　<?= $row["experience"] ?>
+            </div>
             <div class="project_contents">○ ホストの思い</div>
-            <div class="project_contents">　<?= $row["thoughts"] ?></div>
+            <div class="project_contents">　<?= $row["thoughts"] ?>
+            </div>
             <div class="project_contents">○ ツアー時間</div>
             <div class="project_contents">　<?= $row["tour_time"] ?>時間</div>
             <div class="project_contents">○ 価格</div>
@@ -90,7 +98,9 @@ if ($status == false) {
         </div>
         <div class=" project_button">
           <div class="btn"><a href="user_schedule.php" class="btn">戻る</a></div>
-          <div class="btn go"><a href="service_main.php?reservation_id=<?= $row["reservation_id"] ?>" class="btn go">参加</a></div>
+          <div class="btn go"><a
+              href="service_main.php?reservation_id=<?= $row["reservation_id"] ?>&project_id=<?= $row["project_id"] ?>"
+              class="btn go">参加</a></div>
         </div>
       </div>
     </div>
